a23310d8576bb5643b89bcd4c2e1cd7c55261140
easylearningbd <elebdsoft@gmail.com>
2021-12-03 03:59:05 +0600

    e6. Complete Project Setup Part 4

--- middlewares/commons/errorHandler.js
+++ middlewares/commons/errorHandler.js
@@ -9,11 +9,18 @@ function notFoundHandler(req,res,next){
 
 // default error handler 
 function errorHandler(err, req, res, next){
+      res.locals.error = process.env.NODE_ENV === "development" ? err : {message:err.message};
+      res.status(err.status || 500);
 
-    res.render('error',{
-         title: "Error page"
-    });
-
+      if(res.locals.html){
+           // html response
+           res.render("error", {
+                title : "this is html error page",
+           })
+      }else{
+           /// json response
+           res.json(res.locals.error);
+      }
 }
 
 module.exports = {
