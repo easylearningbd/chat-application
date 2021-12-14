acccf099cd17ae962df547d423a511b6fba89960
easylearningbd <elebdsoft@gmail.com>
2021-12-15 03:42:51 +0600

    e16 User Management & Validation Part 5

--- middlewares/users/userValidators.js
+++ middlewares/users/userValidators.js
@@ -1,5 +1,6 @@
 /// external imports 
 const {check, validationResult } = require("express-validator");
+const { unlink } = require("fs");
 const createError = require("http-errors");
 const path = require("path");
 
@@ -49,6 +50,32 @@ const addUserValidators = [
 ];
 
 
+const addUserValidationHandler = function (req, res, next) {
+     const errors = validationResult(req);
+     const mappedErrors = errors.mapped();
+     if(Object.keys(mappedErrors).length === 0){
+          next();
+     }else{
+          // remove uploaded files
+          if(req.files.length > 0) {
+               const {filename} = req.files[0];
+               unlink(
+                    path.join(__dirname, `/../public/uploads/avatars/${filename}`),
+                    (err) => {
+                         if(err) console.log(err);
+                    }
+               );
+           }
+
+           // response the error 
+           res.status(500).json({
+                errors:mappedErrors,
+           });
+     }
+};
+
+
 module.exports = {
-     addUserValidators
+     addUserValidators,
+     addUserValidationHandler,
 };
--- router/usersRouter.js
+++ router/usersRouter.js
@@ -6,13 +6,18 @@ const { getUsers } = require("../controller/usersController");
 const decorateHtmlResponse = require("../middlewares/commons/decorateHtmlResponse");
 
 const avatarUpload = require("../middlewares/users/avatarUpload");
+const { addUserValidators, addUserValidationHandler } = require("../middlewares/users/userValidators");
 
 
 // Login Page 
 router.get("/", decorateHtmlResponse("Users"), getUsers);
 
 /// Add User 
-router.post("/", avatarUpload);
+router.post("/", 
+avatarUpload,
+addUserValidators,
+addUserValidationHandler,
+);
 
 
 module.exports = router; 
