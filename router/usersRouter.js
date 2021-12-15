afce89f647b864274d96aa741b41def69f8aa96b
easylearningbd <elebdsoft@gmail.com>
2021-12-16 05:30:15 +0600

    e24 Delete UserData Part 2

--- router/usersRouter.js
+++ router/usersRouter.js
@@ -2,13 +2,13 @@ const express = require("express");
 const router = express.Router();
 
 // internal imports 
-const { getUsers, addUser } = require("../controller/usersController");
+const { getUsers, addUser,removeUser } = require("../controller/usersController");
 const decorateHtmlResponse = require("../middlewares/commons/decorateHtmlResponse");
 
 const avatarUpload = require("../middlewares/users/avatarUpload");
 const { addUserValidators, addUserValidationHandler } = require("../middlewares/users/userValidators");
 
-
+ 
 // Login Page 
 router.get("/", decorateHtmlResponse("Users"), getUsers);
 
