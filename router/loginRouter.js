40e6fcff578c06e73db9397e5015207e94496628
easylearningbd <elebdsoft@gmail.com>
2021-12-18 03:54:40 +0600

    e27 JWT - Cookie Authentication Part 3

--- router/loginRouter.js
+++ router/loginRouter.js
@@ -2,11 +2,23 @@ const express = require("express");
 const router = express.Router();
 
 // internal imports 
-const { getLogin } = require("../controller/loginController");
+const { getLogin, login } = require("../controller/loginController");
 const decorateHtmlResponse = require("../middlewares/commons/decorateHtmlResponse");
+const { doLoginValidators, doLoginValidationHandler } = require("../middlewares/login/loginValidators");
+
+// set page title 
+const page_title = "Login";
 
 
 // Login Page 
-router.get("/", decorateHtmlResponse("Login"), getLogin);
+router.get("/", decorateHtmlResponse(page_title), getLogin);
+
+router.post("/",  decorateHtmlResponse(page_title), 
+doLoginValidators,
+doLoginValidationHandler,
+login);
+
+
+
 
 module.exports = router;  
